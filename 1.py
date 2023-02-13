cb15e80bd3cbb1e17d3a3197a46c57f6e251a17c
benjiamin911 <965147292@qq.com>
2023-02-12 20:10:27 -0800

    update some message

--- 1.py
+++ 1.py
@@ -2,4 +2,5 @@ from moviepy.editor import VideoFileClip
 
 video = VideoFileClip('C:\\Users\\Stark\\Downloads\\Meditation\\40.mp4')
 audio = video.audio
+print("transfering")
 audio.write_audiofile('C:\\Users\\Stark\\Downloads\\Meditation\\40.mp3')
